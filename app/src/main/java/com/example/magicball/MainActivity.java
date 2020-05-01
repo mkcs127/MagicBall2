4a55edc326e988f5b6c5ff2c6f07ce5db1cbdf60
Mauricio Campos <mcampos@nastec.local>
2020-04-30 22:41:20 -0400

    Se declaran nuevas variables y se avanza con el codigo

--- app/src/main/java/com/example/magicball/MainActivity.java
+++ app/src/main/java/com/example/magicball/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.magicball;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView Ball;
+    private TextView Respuesta;
+
+    private String[] respuestaArray={"Es cierto", "Definitivamente es así", "Sin duda", "Sí definitivamente", "Puede confiar en él", "Como yo lo veo, sí",
+            "Lo más probable", "Perspectiva buena", "Sí", "Las señales apuntan a sí", "Respuesta confusa intente de nuevo", "Pregunte de nuevo más tarde",
+            "Mejor no decirte ahora", "No puedo predecir ahora", "Concéntrate y pregunta de nuevo", "No cuentes con eso",
+            "Mi respuesta es no", "Mis fuentes dicen que no", "Outlook no es tan bueno", "Muy dudoso"}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ball = findViewById(R.id.boton);
+        respuesta = findViewById(R.id.respuesta);
+
+        ball.setOnClickListener(this);
     }
+
 }
