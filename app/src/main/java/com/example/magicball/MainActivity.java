f94283ee04b9f133e0ead098002f15b38c68cec0
Mauricio Campos <mcampos@nastec.local>
2020-05-08 15:17:41 -0400

    Version preliminar, usable.

--- app/src/main/java/com/example/magicball/MainActivity.java
+++ app/src/main/java/com/example/magicball/MainActivity.java
@@ -5,16 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View;
 
+//Libreria Java
+import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
-    private ImageView Ball;
-    private TextView Respuesta;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private ImageView ball;
+    private TextView respuesta;
 
     private String[] respuestaArray={"Es cierto", "Definitivamente es así", "Sin duda", "Sí definitivamente", "Puede confiar en él", "Como yo lo veo, sí",
             "Lo más probable", "Perspectiva buena", "Sí", "Las señales apuntan a sí", "Respuesta confusa intente de nuevo", "Pregunte de nuevo más tarde",
             "Mejor no decirte ahora", "No puedo predecir ahora", "Concéntrate y pregunta de nuevo", "No cuentes con eso",
-            "Mi respuesta es no", "Mis fuentes dicen que no", "Outlook no es tan bueno", "Muy dudoso"}
+            "Mi respuesta es no", "Mis fuentes dicen que no", "Outlook no es tan bueno", "Muy dudoso"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,17 @@ public class MainActivity extends AppCompatActivity {
         respuesta = findViewById(R.id.respuesta);
 
         ball.setOnClickListener(this);
-    }
+        Toast.makeText(MainActivity.this, "Buscando tu destino!!!", Toast.LENGTH_SHORT).show();
 
+    }
+    @Override
+    public void onClick(View v){
+        //creacion de metodo de respuesta aleatorio
+
+        switch (v.getId()) {
+            case R.id.boton:
+                int rand = new Random().nextInt(respuestaArray.length);
+                respuesta.setText(respuestaArray[rand]);
+        }
+    }
 }
